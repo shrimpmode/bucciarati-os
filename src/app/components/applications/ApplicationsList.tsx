d8a1a90e5f98cce96d34606a0991fa30a5817a70
shrimpmode <jsilva@uni.pe>
2024-01-06 21:11:44 -0500

    show active app

--- src/app/components/applications/ApplicationsList.tsx
+++ src/app/components/applications/ApplicationsList.tsx
@@ -3,13 +3,17 @@
 import { apps } from "../applications/apps";
 import { ApplicationIcon } from "./ApplicationIcon";
 import {addApplication} from '@/store/features/application/applicationSlice'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
+import {RootState} from "@/store/store";
+import {Application} from "@/app/types/application";
 
 export const ApplicationsList = () => {
+  const {applications} = useSelector((state:RootState) => state.application)
   const dispatch = useDispatch()
   return (
     <>
       {apps.map((app) => {
+        const isRunning = applications.find((a:Application) => a.metadata.name === app.name);
         return (
           <div
               key={app.name}
@@ -22,8 +26,9 @@ export const ApplicationsList = () => {
                 dispatch(addApplication(app));
               }}
             />
-
-            <div className="w-1 h-1 bg-neutral-50 rounded-full"></div>
+            {
+                isRunning && <div className="w-1 h-1 bg-neutral-300 rounded-full"></div>
+            }
           </div>
         );
       })}
